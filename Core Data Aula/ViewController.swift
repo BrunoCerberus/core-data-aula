e6ca87af3049f72fcca6939a3db6fca3b694e03b
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2017-11-09 23:48:21 -0200

    Initial Commit

---
+++ Core Data Aula/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core Data Aula
+//
+//  Created by Bruno Lopes de Mello on 09/11/2017.
+//  Copyright © 2017 Bruno Lopes de Mello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
