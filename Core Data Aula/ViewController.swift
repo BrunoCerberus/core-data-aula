7bc249761a35e6f74cefda90609b5c8f8748576f
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2017-11-11 15:41:04 -0200

    Algumas funcionalidades adicionadas

--- Core Data Aula/ViewController.swift
+++ Core Data Aula/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        //criar entidade
+        let usuario = NSEntityDescription.insertNewObject(forEntityName: "Usuario", into: context)
+        
+        //configura objeto
+        usuario.setValue("Anna Carolina", forKey: "nome")
+        usuario.setValue(24, forKey: "idade")
+        usuario.setValue("annacarolina", forKey: "login")
+        usuario.setValue("bacon", forKey: "senha")
+        
+        //salvar ou persistir o objeto
+        do {
+            try context.save()
+            print("Dados salvos com sucesso")
+        } catch  {
+            print("Erro ao salvar os dados")
+        }
     }
 
     override func didReceiveMemoryWarning() {
