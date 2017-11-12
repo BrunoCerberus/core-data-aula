ef309565ffd33f636c68d21de8748ea81e73f9e5
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2017-11-12 12:40:34 -0200

    Adicionado funcionalidades como NSSortDescriptors, NSPredicate, NSCompundPredicate

--- Core Data Aula/ViewController.swift
+++ Core Data Aula/ViewController.swift
@@ -18,22 +18,104 @@ class ViewController: UIViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
-        //criar entidade
-        let usuario = NSEntityDescription.insertNewObject(forEntityName: "Usuario", into: context)
+        let requisicao = NSFetchRequest<NSFetchRequestResult>(entityName: "Produto")
         
-        //configura objeto
-        usuario.setValue("Anna Carolina", forKey: "nome")
-        usuario.setValue(24, forKey: "idade")
-        usuario.setValue("annacarolina", forKey: "login")
-        usuario.setValue("bacon", forKey: "senha")
+        //ordenar de A-Z ou Z-A
+        let ordenacaoAZ = NSSortDescriptor(key: "descricao", ascending: true)
+//        let ordenacaoPreco = NSSortDescriptor(key: "preco", ascending: false)
         
-        //salvar ou persistir o objeto
+        //aplicar filtros
+//        let predicate = NSPredicate(format: "descricao == %@", "Agua")
+        let filtroDescricao = NSPredicate(format: "descricao contains [c] %@", "gu")
+//        let predicate = NSPredicate(format: "descricao beginswith [c] %@", "g")
+        let filtroPreco = NSPredicate(format: "preco >= %@", "5.00")
+        
+        let combinacao = NSCompoundPredicate(andPredicateWithSubpredicates: [filtroDescricao,filtroPreco])
+        
+        
+        //Aplicar
+        requisicao.sortDescriptors = [ordenacaoAZ]
+        requisicao.predicate = combinacao
+        
         do {
-            try context.save()
-            print("Dados salvos com sucesso")
-        } catch  {
-            print("Erro ao salvar os dados")
+            let produtos = try context.fetch(requisicao)
+            
+            if produtos.count > 0 {
+                var _contador :Int = 0
+                
+                for produto in produtos as! [NSManagedObject] {
+                    let descricaoProduto = produto.value(forKey: "descricao")
+                    let corProduto = produto.value(forKey: "cor")
+                    let precoProduto = produto.value(forKey: "preco")
+                    
+                    print("Produto \(_contador+1):\nDescricao: \(descricaoProduto ?? "Sem Descricao")\nCor: \(corProduto ?? "Sem Cor")\nPreco: \(precoProduto ?? 0)\n---------------------")
+                    
+                    _contador += 1
+                }
+                
+            } else {
+                print("Nenhum produto encontrado")
+            }
+        } catch {
+            print("Erro ao recuperar produtos")
         }
+        
+//        //criar objeto
+//        let produto = NSEntityDescription.insertNewObject(forEntityName: "Produto", into: context)
+//
+//        produto.setValue("Agua", forKey: "descricao")
+//        produto.setValue("prata", forKey: "cor")
+//        produto.setValue(5.00, forKey: "preco")
+//
+//        do {
+//            try context.save()
+//            print("Dados salvos com sucesso")
+//        } catch {
+//            print("Erro ao salvar")
+//        }
+        
+//        //criar requisicao
+//        let requisicao = NSFetchRequest<NSFetchRequestResult>(entityName: "Usuario")
+//
+//        do {
+//            let usuarios = try context.fetch(requisicao)
+//
+//            if usuarios.count > 0 {
+//                var _contador : Int = 0
+//                for usuario in usuarios as! [NSManagedObject]{
+//                    let nomeUsuario = usuario.value(forKey: "nome")
+//                    let idadeUsuario = usuario.value(forKey: "idade")
+//                    let loginUsuario = usuario.value(forKey: "login")
+//                    let senhaUsuario = usuario.value(forKey: "senha")
+//
+//                    print("Usuario \(_contador+1):\nNome: \(nomeUsuario ?? "Nome de usuario nao encontrado")\nIdade: \(idadeUsuario ?? "Idade de usuario nao encontrada")\nLogin: \(loginUsuario ?? "Login de usuario nao encontrado")\n" +
+//                        "Senha: \(senhaUsuario ?? "Senha de usuario nao encontrada")\n-------------------")
+//                    _contador += 1
+//                }
+//
+//            } else {
+//                print("Nenhum usuario encontrado")
+//            }
+//        } catch {
+//            print("Erro ao recuperar os usuarios")
+//        }
+        
+//        //criar entidade
+//        let usuario = NSEntityDescription.insertNewObject(forEntityName: "Usuario", into: context)
+//
+//        //configura objeto
+//        usuario.setValue("figueiredo", forKey: "nome")
+//        usuario.setValue(42, forKey: "idade")
+//        usuario.setValue("vagabundo", forKey: "login")
+//        usuario.setValue("moveisconquista", forKey: "senha")
+//
+//        //salvar ou persistir o objeto
+//        do {
+//            try context.save()
+//            print("Dados salvos com sucesso")
+//        } catch  {
+//            print("Erro ao salvar os dados")
+//        }
     }
 
     override func didReceiveMemoryWarning() {
